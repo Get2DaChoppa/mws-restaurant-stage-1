7feb48ed9b5cf3d5cc66688175e37cc85916ca9e
Get2DaChoppa <s.g.bibas@gmail.com>
2018-04-01 15:31:26 +0300

    review-2

--- src/js/sw.js
+++ src/js/sw.js
@@ -4,23 +4,23 @@
 //-----Cache the application shell-----//
 
     var filesToCache = [
-        '/../.',
-        '/../css/styles.css',
-        '/../img/1.jpg',
-        '/../img/2.jpg',
-        '/../img/3.jpg',
-        '/../img/4.jpg',
-        '/../img/5.jpg',
-        '/../img/6.jpg',
-        '/../img/7.jpg',
-        '/../img/8.jpg',
-        '/../img/9.jpg',
-        '/../index.html',
-        '/../restaurant.html',
-        '/../data/restaurants.json',
-        '/../js/dbhelper.js',
-        '/../js/main.js',
-        '/../js/restaurant_info.js'
+        '.',
+        'css/styles.css',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'index.html',
+        'restaurant.html',
+        'data/restaurants.json',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js'
     ];
 
 
